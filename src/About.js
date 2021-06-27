9843d0511938486773898888f503af606e459db5
RawanAlakhras <ralakhras36@gmail.com>
2021-06-27 21:55:32 +0300

    add about

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./css/About.css";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import{ Button ,Card}from "react-bootstrap/";
 
 import imgNEmh from "./img/Nemh.jpg";
 import imgRawan from "./img/Rawan.jpg";
@@ -10,23 +10,36 @@ import imgIbrahim from "./img/Ibrahim.jpg";
 import imgAhmad from "./img/Ahmad.jpg";
 import imgNadeen from "./img/Nadeen.jpg";
 import axios from "axios";
-import {AiFillGithub ,AiFillLinkedin} from 'react-icons/all'
+import { AiFillGithub, AiFillLinkedin } from "react-icons/all";
 class About extends Component {
   state = {
     data: [],
+    userName: "",
+    usercomment: "",
   };
 
-  addData = (event) => {
+  addData = async(event) => {
     event.preventDefault();
-    let name = event.target.userName.Value;
-    let usercomment = event.target.comment.Value;
-    let serverUrl = process.env.REACT_APP_SERVER;
+
+    const commentObj={
+      userName: event.target.username.value,
+      usercomment: event.target.comment.value,
+    };
+
+   const server =process.env.REACT_APP_SERVER;
+   const getcomment=`${server}/addComment`;
+   const commentData= await axios.post(getcomment,commentObj);
+   console.log(commentData.data);
+
+
+
+    /*   let serverUrl = process.env.REACT_APP_SERVER;
     let url = `${serverUrl}/feedback`;
 
     axios.post(url, {
       name: name,
       comment: usercomment,
-    });
+    }); */
   };
 
   // handleClose=()=>{
@@ -59,121 +72,107 @@ class About extends Component {
             <div className="row pt-5 pb-5">
               <div className="col">
                 <div className="box">
-                <div className="box-pic">
-                  <div className='thumb'>
-                  <p> <AiFillGithub />  <AiFillLinkedin /></p>
-                  </div> 
-                  <img src={imgIbrahim} alt="ibrahem" />
-                  
-                  
-                  <div className="social">
-                    
+                  <div className="box-pic">
+                    <div className="thumb">
+                      <p>
+                        {" "}
+                        <AiFillGithub /> <AiFillLinkedin />
+                      </p>
+                    </div>
+                    <img src={imgIbrahim} alt="ibrahem" />
+
+                    <div className="social"></div>
                   </div>
-                 
-                </div>
-                <h4>Ibrahem Aldereni</h4>
-                <p>Softwate Development</p>
+                  <h4>Ibrahem Aldereni</h4>
+                  <p>Softwate Development</p>
                 </div>
               </div>
               <div className="col">
-               
                 <div className="box">
-                <div className="box-pic">
-                  <div className='thumb'>
-                  <p> <AiFillGithub />  <AiFillLinkedin /></p>
-                  </div> 
-                  <img src={imgRawan} alt="rawan" />
-                  
-                  
-                  <div className="social">
-                    
+                  <div className="box-pic">
+                    <div className="thumb">
+                      <p>
+                        {" "}
+                        <AiFillGithub /> <AiFillLinkedin />
+                      </p>
+                    </div>
+                    <img src={imgRawan} alt="rawan" />
+
+                    <div className="social"></div>
                   </div>
-                 
+                  <h4>Rawan Alakhras</h4>
+                  <p>Softwate Development</p>
                 </div>
-                <h4>Rawan Alakhras</h4>
-                <p>Softwate Development</p>
-                </div>
-                
               </div>
               <div className="col">
-               
                 <div className="box">
-                <div className="box-pic">
-                  <div className='thumb'>
-                  <p> <AiFillGithub />  <AiFillLinkedin /></p>
-                  </div> 
-                  <img src={imgAhmad} alt="ahmad" />
-                  
-                  
-                  <div className="social">
-                    
+                  <div className="box-pic">
+                    <div className="thumb">
+                      <p>
+                        {" "}
+                        <AiFillGithub /> <AiFillLinkedin />
+                      </p>
+                    </div>
+                    <img src={imgAhmad} alt="ahmad" />
+
+                    <div className="social"></div>
                   </div>
-                 
+                  <h4>Ahamd Omar A. Shalein</h4>
+                  <p>Softwate Development</p>
                 </div>
-                <h4>Ahamd Omar A. Shalein</h4>
-                <p>Softwate Development</p>
-                </div>
-                
               </div>
             </div>
             <div className="row pb-5 pt-5">
               <div className="col">
                 <div className="box">
-                <div className="box-pic">
-                  <div className='thumb'>
-                  <p> <AiFillGithub />  <AiFillLinkedin /></p>
-                  </div> 
-                  <img src={imgNoor} alt="noor" />
-                  
-                  
-                  <div className="social">
-                    
+                  <div className="box-pic">
+                    <div className="thumb">
+                      <p>
+                        {" "}
+                        <AiFillGithub /> <AiFillLinkedin />
+                      </p>
+                    </div>
+                    <img src={imgNoor} alt="noor" />
+
+                    <div className="social"></div>
                   </div>
-                 
-                </div>
-                <h4>Noor Azar</h4>
-                <p>Softwate Development</p>
+                  <h4>Noor Azar</h4>
+                  <p>Softwate Development</p>
                 </div>
               </div>
               <div className="col">
-               
                 <div className="box">
-                <div className="box-pic">
-                  <div className='thumb'>
-                  <p> <AiFillGithub />  <AiFillLinkedin /></p>
-                  </div> 
-                  <img src={imgNadeen} alt="Nadeen" />
-                  
-                  
-                  <div className="social">
-                    
+                  <div className="box-pic">
+                    <div className="thumb">
+                      <p>
+                        {" "}
+                        <AiFillGithub /> <AiFillLinkedin />
+                      </p>
+                    </div>
+                    <img src={imgNadeen} alt="Nadeen" />
+
+                    <div className="social"></div>
                   </div>
-                 
+                  <h4>nadeen shuweihat</h4>
+                  <p>Softwate Development</p>
                 </div>
-                <h4>nadeen shuweihat</h4>
-                <p>Softwate Development</p>
-                </div>
-              
               </div>
               <div className="col">
-           
                 <div className="box">
-                <div className="box-pic">
-                  <div className='thumb'>
-                  <p> <AiFillGithub />  <AiFillLinkedin /></p>
-                  </div> 
-                  <img src={imgNEmh} alt="nemh" />
-                  
-                  
-                  <div className="social">
-                    
+                  <div className="box-pic">
+                    <div className="thumb">
+                      <p>
+                        {" "}
+                        <AiFillGithub /> <AiFillLinkedin />
+                      </p>
+                    </div>
+                    <img src={imgNEmh} alt="nemh" />
+
+                    <div className="social"></div>
                   </div>
-                 
+                  <h4>Nemh Ablan</h4>
+                  <p>Softwate Development</p>
                 </div>
-                <h4>Nemh Ablan</h4>
-                <p>Softwate Development</p>
-                </div>
-            
               </div>
             </div>
           </div>
@@ -181,29 +180,48 @@ class About extends Component {
 
         {/* ======================== */}
 
-       
         {/* form sec */}
-        <div>
-          <Form onSubmit={this.addData}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Enter Your Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Your Name"
-                name="username"
-              />
-              <Form.Text className="text-muted">
-                Please Add Your FeedBack
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Add Your FeedBack</Form.Label>
-              <Form.Control type="text" placeholder="Comment" name="comment" />
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={this.handleClose}>
-              Comment
-            </Button>
-          </Form>
+        <div className="container pt-5 pb-5 mt-5 mb-5">
+          <div className="row mb-5 row-cols-2">
+   
+            <div className="col">
+              <Card className='commentCard'>
+                <Card.Header>{this.state.userName}</Card.Header>
+                <Card.Body>
+                  <Card.Text>
+                   {this.state.usercomment}
+                  </Card.Text>
+                  
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col">
+              <Form onSubmit={this.addData} className="about-form">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Enter Your Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Your Name"
+                    name="username"
+                  />
+                  <Form.Text className="text-muted">
+                    Please Add Your FeedBack
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="comment">
+                  <Form.Label>Your feedback</Form.Label>
+                  <Form.Control as="textarea" rows={3} name="comment" />
+                </Form.Group>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={this.handleClose}
+                >
+                  Comment
+                </Button>
+              </Form>
+            </div>
+          </div>
         </div>
       </div>
     );
