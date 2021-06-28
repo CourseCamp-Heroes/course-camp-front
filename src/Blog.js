7ea2991cd96956910970e10a2fb6ac2c595db49b
AhmadShalein <ahmadshalein@gmail.com>
2021-06-28 18:00:58 +0300

    Finish blog and about us pages

--- src/Blog.js
+++ src/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Card, Button} from 'react-bootstrap/';
+import {Card, Button, Spinner} from 'react-bootstrap/';
 import "./css/Blog.css";
 import axios from 'axios';
 
@@ -40,23 +40,45 @@ class Blog extends Component {
               <div className='blog'>
                <div className='container'>
                  <h1>Blog Page</h1>
-                 <p>Hero Course Camp We gathered to build a Website to help our community to to learn about programming free charges.</p>
+                 <p>Hero Course Camp We gathered to build a Website to help our community to learn about programming free charges.</p>
                </div>
               </div>
-              {this.state.error ? <p>{this.state.error}</p> : 
-              this.state.blogData.map((item,index) => {
+              {this.state.blogData.length === 0 ? (
+            <div className="spinner">
+              <Spinner animation="border" variant="success" />
+              <p style={{ color: "green" }}>Loading...</p>
+            </div>
+          ) : (
+            <div className='container mt-5 pt-5'>
+            <div className='row row-cols-3 mb-5'>
+            
+
+              {this.state.blogData.map((item,index) => {
                 return (
-                  <Card className="blogCard">
-                  <Card.Body>
-                      <Card.Title className="blogTitle">{item.title}</Card.Title>
-                      <Card.Text>{item.description}</Card.Text>
-                      <Card.Img className="blogImg" variant="top" src={item.image} />
-                      <a href={item.url} target="_blank" className="btn btn-success header-btn btn-blog mt-5">Read More</a>
-                      {/* <Button onClick={item.url} target="_blank" type="Read More" value="Read More" />{' '}*/}
-                      </Card.Body>
-                  </Card>
+                   <div className='col mb-5'>
+                      <Card className="blogCard">
+                    <Card.Body>
+                        <Card.Title className="blogTitle">{item.title}</Card.Title>
+                        <Card.Text>{item.description}</Card.Text>
+                        <Card.Img className="blogImg" variant="top" src={item.image} />
+                         
+                        <a href={item.url} target="_blank" className="btn btn-success header-btn btn-blog mt-5">Read More</a>
+                        {/* <Button onClick={item.url} target="_blank" type="Read More" value="Read More" />{' '}*/}
+                        </Card.Body>
+                       
+                    </Card>
+                   </div>
                 )
-                })}
+                })
+               }
+             
+             </div>
+             </div>
+             
+              )
+              
+              
+              }
             </div>
     )}
 }
