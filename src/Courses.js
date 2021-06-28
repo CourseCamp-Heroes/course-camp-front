8ba297698a561f5907115829b57a6dce9c17e99b
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-28 12:06:18 +0300

    add courses canvas

--- src/Courses.js
+++ src/Courses.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Card, Spinner } from "react-bootstrap";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import "./css/Courses.css";
 
 import axios from "axios";
@@ -18,6 +20,8 @@ class Courses extends Component {
     skillsValue: "all",
     // end of select 3 filterations
     searchValue: "",
+    // offcanvas:
+    showCanvas: false,
   };
 
   componentDidMount = () => {
@@ -287,11 +291,21 @@ class Courses extends Component {
     });
     this.setState({ filterdCourses: newdata });
   };
-  // end of search input function
 
   searchChange = (e) => {
     this.setState({ searchValue: e.target.value });
   };
+  // end of search input function
+
+  // start of canvas functions
+  handleShow = () => {
+    this.setState({ showCanvas: true });
+  };
+
+  handleClose = () => {
+    this.setState({ showCanvas: false });
+  };
+  // end of canvas functions
 
   render() {
     return (
@@ -373,19 +387,38 @@ class Courses extends Component {
           ) : (
             this.state.filterdCourses.map((course, i) => {
               return (
-                <Card className="course-card" key={i}>
-                  <Card.Img
-                    variant="top"
-                    src={course.image}
-                    style={{ height: 160 }}
-                  />
-                  <Card.Body>
-                    <Card.Title className="pb-2">{course.title}</Card.Title>
-                    <Card.Text>{course.subtitle}</Card.Text>
-                    <Card.Text>{course.level}</Card.Text>
-                    <Card.Text>{course.duration}</Card.Text>
-                  </Card.Body>
-                </Card>
+                <>
+                  <Card
+                    className="course-card"
+                    key={i}
+                    onClick={this.handleShow}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={course.image}
+                      style={{ height: 160 }}
+                    />
+                    <Card.Body>
+                      <Card.Title className="pb-2">{course.title}</Card.Title>
+                      <Card.Text>{course.subtitle}</Card.Text>
+                      <Card.Text>{course.level}</Card.Text>
+                      <Card.Text>{course.duration}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                  // offcanvas
+                  <Offcanvas
+                    show={this.state.showCanvas}
+                    onHide={this.handleClose}
+                  >
+                    <Offcanvas.Header closeButton>
+                      <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                      Some text as placeholder. In real life you can have the
+                      elements you have chosen. Like, text, images, lists, etc.
+                    </Offcanvas.Body>
+                  </Offcanvas>
+                </>
               );
             })
           )}
